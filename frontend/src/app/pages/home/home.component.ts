bb6692c7bec70c82edbb3c11d222351fdd6427bd
Marta Altimir <martaag18@gmail.com>
2025-02-20 16:51:57 +0100

    Upload footer component

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -3,9 +3,10 @@ import { ReviewsComponent } from './reviews/reviews.component';
 import { ExercicesComponent } from './exercices/exercices.component';
 import { WebinarsComponent } from './webinars/webinars.component';
 import { HeroComponent } from "./hero/hero.component";
+import { CTAComponent } from './cta/cta.component';
 @Component({
   selector: 'app-home',
-  imports: [ReviewsComponent, ExercicesComponent, WebinarsComponent, HeroComponent],
+  imports: [ReviewsComponent, ExercicesComponent, WebinarsComponent, HeroComponent, CTAComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
