e730e83902e6f149e99f6ce612738f1f1b3aafb2
Marta Altimir <martaag18@gmail.com>
2025-02-20 11:24:04 +0100

    Update lading page + create components for a modular approach

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ReviewsComponent } from '../reviews/reviews.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ReviewsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
