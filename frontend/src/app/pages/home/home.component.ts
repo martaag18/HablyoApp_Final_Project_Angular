fd1b0c9017bbfddf63524b63a652c81b55d09a13
Marta Altimir <martaag18@gmail.com>
2025-02-20 15:55:19 +0100

    Divide home into homponents for a modular approach

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { ReviewsComponent } from '../reviews/reviews.component';
-
+import { ReviewsComponent } from './reviews/reviews.component';
+import { ExercicesComponent } from './exercices/exercices.component';
+import { WebinarsComponent } from './webinars/webinars.component';
+import { HeroComponent } from "./hero/hero.component";
 @Component({
   selector: 'app-home',
-  imports: [ReviewsComponent],
+  imports: [ReviewsComponent, ExercicesComponent, WebinarsComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ frontend/src/app/pages/home/webinars/webinars.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-webinars',
+  imports: [],
+  templateUrl: './webinars.component.html',
+  styleUrl: './webinars.component.scss'
+})
+export class WebinarsComponent {
+
+}
