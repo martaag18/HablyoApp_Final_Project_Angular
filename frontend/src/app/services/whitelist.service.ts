c916fdd8faf68372273e923de07061d593408c54
Marta Altimir <martaag18@gmail.com>
2025-02-22 10:39:37 +0100

    Add whitelist service

---
+++ frontend/src/app/services/whitelist.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WhitelistService {
+
+  private http = inject(HttpClient)
+  private API_URL = "http://localhost:3000/whitelist";
+
+  checkEmail(email: string): Observable <{ email: string; whitelisted: boolean }> {
+    const query = `?email=${encodeURIComponent(email.trim().toLowerCase())}`;
+    return this.http.get<{ email: string; whitelisted: boolean }>(this.API_URL + query);
+  }
+}
