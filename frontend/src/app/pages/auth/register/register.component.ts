e25b1f1819daf7a30676ceb4e53fba6b3c74d779
Marta Altimir <martaag18@gmail.com>
2025-02-22 11:57:40 +0100

    Upload frontend to enable registration

--- frontend/src/app/pages/auth/register/register.component.ts
+++ frontend/src/app/pages/auth/register/register.component.ts
@@ -9,7 +9,9 @@ import { inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WhitelistService } from '../../../services/whitelist.service';
 import { passwordMatchValidator } from '../../../shared/validators/custom-validators.component';
-
+import { Register } from '../../../shared/interfaces/register.interface';
+import { RegisterService } from '../../../services/register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -18,17 +20,17 @@ import { passwordMatchValidator } from '../../../shared/validators/custom-valida
   styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-
   private whitelistService = inject(WhitelistService);
+  private registerService = inject(RegisterService);
+  private router = inject(Router);
 
   currentStep: number = 1;
   emailNotAllowed = false;
 
-
   registerForm = new FormGroup(
     {
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl('', [Validators.required]),
       age: new FormControl('', [Validators.required, Validators.min(1)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
@@ -36,9 +38,17 @@ export class RegisterComponent {
     },
     { validators: [passwordMatchValidator] }
   );
-  
 
-  submitRegister() {
+  submitRegister(): void {
+    //Verificar si formulario es válido
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+    } else {
+      this.checkWhitelist();
+    }
+  }
+
+  private checkWhitelist(): void {
     const emailValue = this.registerForm.get('email')?.value;
     if (emailValue) {
       this.whitelistService.checkEmail(emailValue).subscribe({
@@ -47,7 +57,7 @@ export class RegisterComponent {
             this.emailNotAllowed = true;
           } else {
             this.emailNotAllowed = false;
-            console.log("Email con acceso")
+            this.createUser();
           }
         },
         error: (err) => {
@@ -56,6 +66,35 @@ export class RegisterComponent {
         },
       });
     }
+  }
+
+  private createUser(): void {
+    //Convertir age a number
+    const ageValue = this.registerForm.value.age!;
+    const ageNumber = Number(ageValue); 
+
+    const data: Register = {
+      name: this.registerForm.value.name!,
+      surname: this.registerForm.value.surname!,
+      age: ageNumber,
+      email: this.registerForm.value.email!,
+      password: this.registerForm.value.password!,
+      repeatPassword: this.registerForm.value.repeatPassword!,
+    };
+    console.log('submitRegister() invocado'); 
+
+    this.registerService.registerUser(data).subscribe({
+      next: (response) => {
+        console.log('Registro exitoso', response.message);
+        alert('¡Registro exitoso! Ahora ya te puedes loguear');
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Error al registrar usuario', err);
+        console.error('Mensajes de validación:', err.error.message);
+        alert(err.error.message || 'Registration failed');
+      },
+    });
   }
 
   goToNextStep(): void {
---
+++ frontend/src/app/services/register.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { inject } from '@angular/core';
+import { Register } from '../shared/interfaces/register.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  private http = inject(HttpClient)
+  private API_URL = "http://localhost:3000/users";
+
+  registerUser (data: Register) : Observable<{message: string}> {
+    console.log('Enviando datos a POST /users:', data);
+    return this.http.post<{message:string}>(this.API_URL, data);
+  }
+}
