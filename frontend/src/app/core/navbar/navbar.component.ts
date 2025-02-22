13c5d4608a2004a7e2a66bb490ab39a82b89cd9a
Marta Altimir <martaag18@gmail.com>
2025-02-22 10:43:12 +0100

    Refactor navigation: updated app.routes, navbar, and config

--- frontend/src/app/core/navbar/navbar.component.ts
+++ frontend/src/app/core/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
 import { Component, ElementRef, HostListener } from '@angular/core';
 import { inject } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
