9e7cfe1b5c4d9a527780d3e1813ada23903b15fe
Marta Altimir <martaag18@gmail.com>
2025-02-24 19:14:36 +0100

    refactor(webinars): improve layout & logic for upcoming webinars display

--- frontend/src/app/pages/home/webinars/webinars.component.ts
+++ frontend/src/app/pages/home/webinars/webinars.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-webinars',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './webinars.component.html',
   styleUrl: './webinars.component.scss'
 })
