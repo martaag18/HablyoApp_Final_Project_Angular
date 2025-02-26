c14c4efd9b41f52ed08c91f4593b8f445ffefaac
Marta Altimir <martaag18@gmail.com>
2025-02-26 13:33:51 +0100

    Apply Lazy load

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LoginComponent } from './pages/auth/login/login.component';
-import { ExercicesComponent } from './pages/exercices/exercices.component';
 import { UpcomingWebinarsComponent } from './pages/webinars/upcoming-webinars/upcoming-webinars.component';
 import { WebinarSignupComponent } from './pages/webinars/webinar-signup/webinar-signup.component';
 
@@ -10,8 +9,14 @@ export const routes: Routes = [
     { path: "", component: HomeComponent},
     { path: "register", component: RegisterComponent},
     { path: "login", component: LoginComponent},
-    { path: "exercices", component: ExercicesComponent},
     { path: "webinars", component: UpcomingWebinarsComponent},
     { path: "webinar-signup", component: WebinarSignupComponent},
+  {
+    path: 'exercise',
+    loadComponent: () =>
+      import('./pages/exercises/components/exercise-container/exercise-container.component').then(
+        m => m.ExerciseContainerComponent
+      ),
+  },
 
 ];
