4d524149b8309def08537a31f2153245a41b85be
Marta Altimir <martaag18@gmail.com>
2025-02-26 10:23:10 +0100

    Update hero component

--- frontend/src/app/pages/home/hero/hero.component.ts
+++ frontend/src/app/pages/home/hero/hero.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
