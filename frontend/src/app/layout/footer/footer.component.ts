2d35bb72840b67bb9bbc76785fc93db1218a92ec
Marta Altimir <martaag18@gmail.com>
2025-02-27 09:38:50 +0100

    Reafactor footer to avoid hardcoding

--- frontend/src/app/layout/footer/footer.component.ts
+++ frontend/src/app/layout/footer/footer.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.css'] // o .scss según convenga
 })
 export class FooterComponent {
 
+  //Images
+  logoUrl = 'assets/Logos/hablyo_red_logo.png';
+  websiteUrl = 'https://www.hablyo.com/';
+  email = 'info@hablyo.com';
+  phone = '+34 611 190 499';
+  instagramUrl = 'https://instagram.com/hablyo';
+  whatsappUrl = 'https://wa.me/34611190499';
+
+  // links
+  policyLinks = [
+    { label: 'Aviso Legal', url: '/aviso-legal' },
+    { label: 'Política de Cookies', url: '/cookies' },
+    { label: 'Política de Privacidad', url: '/privacidad' }
+  ];
 }
+
