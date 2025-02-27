232d080520101f8e87a5e79ca20df48850c13e99
Marta Altimir <martaag18@gmail.com>
2025-02-27 10:07:16 +0100

    Add shared components to modularize

---
+++ frontend/src/app/shared/constants/navbar-links.constants.ts
@@ -0,0 +1,14 @@
+export const MOBILE_MENU_LINKS = [
+    { label: 'Inicio', routerLink: '/' },
+    { label: 'Webinars', routerLink: '/webinars' },
+    { label: 'Ejercicios', routerLink: '/ejercicios' },
+    { label: 'Área personal', routerLink: '/login', icon: 'fa-solid fa-arrow-right-to-bracket' },
+    { label: 'Registrarse', routerLink: '/registro-usuario', icon: 'fas fa-user' }
+  ];
+  
+  export const MAIN_LINKS = [
+    { label: 'INICIO', routerLink: '/' },
+    { label: 'WEBINARS', routerLink: '/webinars' },
+    { label: 'EJERCICIOS', routerLink: '/ejercicios' },
+  ];
+  
---
+++ frontend/src/app/shared/ui/my-button/my-button.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-my-button',
+  imports: [RouterLink, CommonModule],
+  templateUrl: './my-button.component.html',
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent {
+
+  @Input() label: string = 'Click';
+  @Input() color: 'primary' | 'secondary' = 'primary';
+  @Input() disabled: boolean = false;
+  @Input() routerLink?: string | any[];
+
+  //Evento que se dispara cuando se hace clic en el botón
+  @Output() clicked = new EventEmitter<void>();
+
+  onClick() {
+    // Se avisa al padre que hacemos click
+    this.clicked.emit();
+  }
+
+  computeClasses(): string {
+    const baseClasses = 'cursor-pointer text-white px-6 py-3 rounded-full font-bold transition-all duration-300';
+  
+    if (this.color === 'primary') {
+      return `${baseClasses} bg-primary hover:bg-[#d73232]`;
+    } else if (this.color === 'secondary') {
+      return `${baseClasses} bg-secondary hover:bg-[#2cb8a9]`;
+    } else if (this.color === 'tertiary') {
+      return `${baseClasses} bg-tertiary hover:bg-[#d9a300]`;
+    } else {
+      return `${baseClasses} bg-gray-600 hover:bg-gray-700`;
+    }
+  }  
+}
+
+
